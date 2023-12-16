779d305d44ee1df2179ae2f86d9194014e0a6fd4
jobkilonzo <jobkilonzo95@gmail.com>
2023-12-17 00:45:45 +0300

    Added gsap functionalities

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import gsap from "gsap";
 
+import { useGSAP } from "@gsap/react";
+import Porotofolio from "./component/Porotofolio";
+import Home from "./component/Home";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Orange from "./component/Orange";
+gsap.registerPlugin(ScrollTrigger)
+gsap.defaults({ ease: "none", duration: 2 })
 function App() {
+  const container = useRef()
+
+  const portofolio = useRef();
+  const contact = useRef()
+
+  useGSAP(() => {
+    // use selectors...
+    //gsap.to(".home", { rotation: "+=360" });
+
+    const t1 = gsap.timeline()
+    
+    t1.from(".portofolio", { xPercent: -100 })
+    t1.from(".contact", { xPercent: 100 })
+
+    ScrollTrigger.create({
+      animation: t1,
+      trigger: ".container",
+      start: "top top",
+      end: "+=1200px",
+      pinSpacing: false,
+      scrub: true,
+      pin: true,
+      anticipatePin: 1
+    })
+
+
+
+    // or refs...
+    //gsap.to(portofolio.current, { rotation: "-=360" });
+
+  }, { scope: container }); // <-- scope for selector text (optional)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={container} className="App">
+      <div  className="home"><Home /></div>
+      <div ref={portofolio} className=" portofolio bg-white text-black"><Porotofolio /></div>
+      <div ref={contact} className="contact bg-orange-500"><Orange /></div>
     </div>
   );
 }
---
+++ src/component/Orange.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Orange = () => {
+  return (
+    <div className='h-screen w-full max-w-[1240px] mx-auto text-center flex flex-col justify-center'>
+        <p>
+        I'm a dedicated software engineer fueled by a passion for turning
+         intricate ideas into functional, user-centric solutions. Proficient in various programming languages and armed with a keen eye for detail, I specialize in creating robust, scalable applications. My experience spans both front-end and back-end development, allowing me to craft seamless experiences while ensuring efficient system architecture. I thrive in collaborative environments, embracing challenges as opportunities to learn and innovate. My goal is to continuously evolve, contributing positively to technological advancements
+         while delivering impactful solutions that resonate with users.
+        </p>
+    </div>
+  )
+}
+
+export default Orange
